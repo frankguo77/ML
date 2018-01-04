6f0e892aea89d6aa2f440fa4dfe4005d2c623c53
frankguo77 <guozhi824@163.com>
2018-01-04 23:27:31 +0800

    Add read data

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,36 +1,70 @@
 import numpy as np
+import csv
+
 
 # m denotes the number of examples here, not the number of features
 def gradientDescent(x, y, theta, alpha, m, numIterations):
-     xTrans = x.transpose()
-     print(xTrans)
-     #print(x)
-     for i in range(0, numIterations):
+    # xTrans = x.transpose()
+    xTrans = x.T
+    # print(xTrans)
+    # print(x)
+    for i in range(0, numIterations):
         hypothesis = np.dot(x, theta)
-        #print(hypothesis)
+        # print(hypothesis)
         loss = hypothesis - y
         # avg cost per example (the 2 in 2*m doesn't really matter here.
         # But to be consistent with the gradient, I include it)
         cost = np.sum(loss ** 2) / (2 * m)
-        #print("Iteration %d | Cost: %f" % (i, cost))
+        # print("Iteration %d | Cost: %f" % (i, cost))
         # avg gradient per example
         gradient = np.dot(xTrans, loss) / m
         # update
         theta = theta - alpha * gradient
-     return theta
+    return theta
 
-X = np.array([[41.9,43.4,43.9,44.5,47.3,47.5,47.9,50.2,52.8,53.2,56.7,57.0,63.5,65.3,71.1,77.0,77.8], [29.1,29.3,29.5,29.7,29.9,30.3,30.5,30.7,30.8,30.9,31.5,31.7,31.9,32.0,32.1,32.5,32.9]])
-#X = np.array([41.9,43.4,43.9,44.5,47.3,47.5,47.9,50.2,52.8,53.2,56.7,57.0,63.5,65.3,71.1,77.0,77.8])
-y = np.array([251.3,251.3,248.3,267.5,273.0,276.5,270.3,274.9,285.0,290.0,297.0,302.5,304.5,309.3,321.7,330.7,349.0])
+
+def readTainningData():
+    data = []
+    # 每一個維度儲存一種污染物的資訊
+    for i in range(18):
+        data.append([])
+
+    n_row = 0
+    text = open('Data/train.csv', 'r', encoding='big5')
+    row = csv.reader(text, delimiter=",")
+    for r in row:
+        # 第0列沒有資訊
+        if n_row != 0:
+            # 每一列只有第3-27格有值(1天內24小時的數值)
+            for i in range(3, 27):
+                if r[i] != "NR":
+                    data[(n_row - 1) % 18].append(float(r[i]))
+                else:
+                    data[(n_row - 1) % 18].append(float(0))
+        n_row = n_row + 1
+    text.close()
+    return data
+
+trainingdata = readTainningData()
+print(trainingdata)
+
+'''
+X = np.array([[41.9, 43.4, 43.9, 44.5, 47.3, 47.5, 47.9, 50.2, 52.8, 53.2, 56.7, 57.0, 63.5, 65.3, 71.1, 77.0, 77.8],
+              [29.1, 29.3, 29.5, 29.7, 29.9, 30.3, 30.5, 30.7, 30.8, 30.9, 31.5, 31.7, 31.9, 32.0, 32.1, 32.5, 32.9]])
+# X = np.array([41.9,43.4,43.9,44.5,47.3,47.5,47.9,50.2,52.8,53.2,56.7,57.0,63.5,65.3,71.1,77.0,77.8])
+y = np.array(
+    [251.3, 251.3, 248.3, 267.5, 273.0, 276.5, 270.3, 274.9, 285.0, 290.0, 297.0, 302.5, 304.5, 309.3, 321.7, 330.7,
+     349.0])
 n = np.max(X.shape)
-#print(n)
+# print(n)
 x = np.vstack([np.ones(n), X]).T
-#print(x)
+# print(x)
 m, n = np.shape(x)
-print(m,n)
-numIterations= 100000
+print(m, n)
+numIterations = 100000
 alpha = 0.000005
 theta = np.ones(n)
 print(theta)
 theta = gradientDescent(x, y, theta, alpha, m, numIterations)
 print(theta)
+'''
