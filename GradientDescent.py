6c39cae7ddabbf7f5a25d3edaa40e0a83c8fb795
frankguo77 <guozhi824@163.com>
2018-01-08 22:39:25 +0800

    Add data and source code.

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,27 +1,47 @@
 import numpy as np
 import csv
-
+import math
 
 # m denotes the number of examples here, not the number of features
-def gradientDescent(x, y, theta, alpha, m, numIterations):
-    # xTrans = x.transpose()
-    xTrans = x.T
+def gradientDescent(x, y, theta, alpha, numIterations):
+    print('gradientDescent:')
+    xTrans = x.transpose()
+    #xTrans = x.T
     # print(xTrans)
-    # print(x)
+    print(x.shape)
     for i in range(0, numIterations):
         hypothesis = np.dot(x, theta)
-        # print(hypothesis)
+        #print(hypothesis.shape)
         loss = hypothesis - y
+       #print(loss.shape)
         # avg cost per example (the 2 in 2*m doesn't really matter here.
         # But to be consistent with the gradient, I include it)
-        cost = np.sum(loss ** 2) / (2 * m)
+        cost = np.sum(loss ** 2)/len(x)
         # print("Iteration %d | Cost: %f" % (i, cost))
         # avg gradient per example
-        gradient = np.dot(xTrans, loss) / m
+        gradient = np.dot(xTrans, loss)
+        #print(gradient.shape)
         # update
         theta = theta - alpha * gradient
+        print('iteration: %d | Cost: %f  ' % (i, cost))
     return theta
 
+def adagraDescent(x, y, w, alpha, numIterations):
+    print('AdagradientDescent:')
+    x_t = x.transpose()
+    s_gra = np.zeros(len(x[0]))
+
+    for i in range(numIterations):
+        hypo = np.dot(x, w)
+        loss = hypo - y
+        cost = np.sum(loss ** 2) / len(x)
+        cost_a = math.sqrt(cost)
+        gra = np.dot(x_t, loss)
+        s_gra += gra ** 2
+        ada = np.sqrt(s_gra)
+        w = w - l_rate * gra / ada
+        print('iteration: %d | Cost: %f  ' % (i, cost_a))
+    return w
 
 def readTainningData():
     data = []
@@ -45,26 +65,97 @@ def readTainningData():
     text.close()
     return data
 
-trainingdata = readTainningData()
-print(trainingdata)
+def parseData(data):
+    x = []
+    y = []
+    # 每 12 個月
+    for i in range(12):
+        # 一個月取連續10小時的data可以有471筆
+        for j in range(471):
+            x.append([])
+            # 18種污染物
+            for t in range(18):
+                # 連續9小時
+                for s in range(9):
+                    x[471 * i + j].append(data[t][480 * i + j + s])
+            y.append(data[9][480 * i + j + 9])
+    x = np.array(x)
+    y = np.array(y)
 
-'''
-X = np.array([[41.9, 43.4, 43.9, 44.5, 47.3, 47.5, 47.9, 50.2, 52.8, 53.2, 56.7, 57.0, 63.5, 65.3, 71.1, 77.0, 77.8],
-              [29.1, 29.3, 29.5, 29.7, 29.9, 30.3, 30.5, 30.7, 30.8, 30.9, 31.5, 31.7, 31.9, 32.0, 32.1, 32.5, 32.9]])
-# X = np.array([41.9,43.4,43.9,44.5,47.3,47.5,47.9,50.2,52.8,53.2,56.7,57.0,63.5,65.3,71.1,77.0,77.8])
-y = np.array(
-    [251.3, 251.3, 248.3, 267.5, 273.0, 276.5, 270.3, 274.9, 285.0, 290.0, 297.0, 302.5, 304.5, 309.3, 321.7, 330.7,
-     349.0])
-n = np.max(X.shape)
-# print(n)
-x = np.vstack([np.ones(n), X]).T
-# print(x)
-m, n = np.shape(x)
-print(m, n)
-numIterations = 100000
-alpha = 0.000005
-theta = np.ones(n)
-print(theta)
-theta = gradientDescent(x, y, theta, alpha, m, numIterations)
-print(theta)
-'''
+    # add square term
+    # x = np.concatenate((x,x**2), axis=1)
+
+    # add bias
+    x = np.concatenate((np.ones((x.shape[0], 1)), x), axis=1)
+    print(x.shape)
+
+    return x,y
+
+def readTestData():
+    test_x = []
+    n_row = 0
+    text = open('data/test.csv', "r")
+    row = csv.reader(text, delimiter=",")
+
+    for r in row:
+        if n_row % 18 == 0:
+            test_x.append([])
+            for i in range(2, 11):
+                test_x[n_row // 18].append(float(r[i]))
+        else:
+            for i in range(2, 11):
+                if r[i] != "NR":
+                    test_x[n_row // 18].append(float(r[i]))
+                else:
+                    test_x[n_row // 18].append(0)
+        n_row = n_row + 1
+    text.close()
+    test_x = np.array(test_x)
+
+    # add square term
+    # test_x = np.concatenate((test_x,test_x**2), axis=1)
+
+    # add bias
+    test_x = np.concatenate((np.ones((test_x.shape[0], 1)), test_x), axis=1)
+    return test_x
+
+def saveResult(resulstfile,test_x,theta):
+    print(theta)
+    ans = []
+    for i in range(len(test_x)):
+        ans.append(["id_" + str(i)])
+        a = np.dot(theta, test_x[i])
+        ans[i].append(a)
+
+    filename = "result/" + resulstfile
+    text = open(filename, "w+")
+    s = csv.writer(text, delimiter=',', lineterminator='\n')
+    s.writerow(["id", "value"])
+    for i in range(len(ans)):
+        s.writerow(ans[i])
+    text.close()
+
+rawdata = readTainningData()
+#print(rawdata)
+
+x, y = parseData(rawdata)
+
+theta = np.zeros(len(x[0]))
+#print(theta.shape)
+l_rate = 0.00000001
+repeat = 10000
+
+#theta = gradientDescent(x, y, theta, l_rate,repeat)
+test_x = readTestData()
+print(test_x)
+print(test_x.dtype)
+
+#saveResult('normalResult.csv',test_x)
+
+
+theta = np.zeros(len(x[0]))
+#print(theta.shape)
+theta = adagraDescent(x, y, theta, l_rate,repeat)
+
+saveResult('adaresult.csv',test_x,theta)
+
